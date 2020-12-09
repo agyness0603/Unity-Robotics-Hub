25c64874f7f2e842344f24b7477b8629c0c39aa6
Hamid Younesy <hamid.younesy@unity3d.com>
2020-12-08 16:08:13 -0800

    added system graph scenes with camera sensors

---
+++ tutorials/robotic_sensors/RoboticSensorsProject/Assets/Scripts/GUIDCleaner.cs
@@ -0,0 +1,54 @@
+﻿using Mechatronics.SystemGraph;
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+public class GUIDCleaner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CleanGUID()
+    {
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+        foreach (GameObject go in allObjects)
+        {            
+            GameObjectGUID[] guids = go.GetComponents<GameObjectGUID>();
+            foreach (var guid in guids)
+            {
+                Debug.Log("Removing GameObjectGUID from " + go.name);
+                GameObject.DestroyImmediate(guid);
+            }
+        }
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(GUIDCleaner))]
+public class GUIDCleanerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Clean"))
+        {
+            GUIDCleaner cleaner = (GUIDCleaner)target;
+            cleaner.CleanGUID();
+        }
+    }
+}
+#endif
+
---
+++ tutorials/robotic_sensors/RoboticSensorsProject/Assets/UnityCarHDRP/Readme.txt
@@ -0,0 +1,7 @@
+UnityCar_EdyVehiclePhysics Prefab
+Notice: Car rig is from Edys Vehicle Physics
+https://assetstore.unity.com/packages/tools/physics/edy-s-vehicle-physics-403
+This is not a free asset, so do not distribute for use in production projects (just for demos)
+
+UnityCar_SimpleCarController Prefab
+Notice: Simple car rig made by Unity and free to distribute. Be sure to delete scripts from Edy Vehicle Physics first though (EVP Namespace).
